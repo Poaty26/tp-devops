e6895192401c2bf33d5e3ae7452ea35872d96781
Poaty26 <votre@jcpoaty64@gmail.com>
2026-03-30 16:08:59 +0200

    Exercice 2 : modification app.py - Hello B3 DevOps

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-#Fichier app.py
-print("Hello DevOps")
-print("Version 2")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello B3 DevOps"
+
+app.run(host="0.0.0.0", port=5000)
