1de40d194423e9f4798975e82e78cf32492b5c86
Poaty26 <votre@jcpoaty64@gmail.com>
2026-04-01 12:52:15 +0200

    Fix : rollback app.py vers version stable Hello DevOps

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    time.sleep(0.1)  # Simule un traitement lent
-    return "Hello DevOps slow version"
+    return "Hello DevOps"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
