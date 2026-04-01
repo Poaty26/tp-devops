465960ab08cee669cc4426988cd78211e736bef0
Poaty26 <votre@jcpoaty64@gmail.com>
2026-04-01 12:33:12 +0200

    TP7 : Load testing - ajout v1.1, v1.2, rapport perf_report.txt

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return "Hello DevOps"
+    time.sleep(0.1)  # Simule un traitement lent
+    return "Hello DevOps slow version"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
